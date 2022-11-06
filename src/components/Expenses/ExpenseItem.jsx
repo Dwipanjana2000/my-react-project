a3b1f384d46565013e136b91c75ffdfe3a5eb6b7
Dwipanjana Das <dwipanjanadas785@gmail.com>
2022-11-07 00:34:29 +0530

    third commit

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Card from "./Card";
-import "./Card.css";
+import Card from "../UI/Card";
+import "../UI/Card.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails"
 import "./ExpenseItem.css"
@@ -8,15 +8,15 @@ import "./ExpenseItem.css"
 const ExpenseItem = (props) => {
 
   return (
-    
-     <Card className="expense-item"><h1>
+
+    <Card className="expense-item"><h1>
       <ExpenseDate date={props.date}></ExpenseDate>
-     </h1>
-     <div className="expense-item__description">
-      <h2><ExpenseDetails title={props.title} amount={props.amount}></ExpenseDetails> </h2>
+    </h1>
+      <div className="expense-item__description">
+        <h2><ExpenseDetails title={props.title} amount={props.amount}></ExpenseDetails> </h2>
       </div>
     </Card>
-   
+
   );
 };
 
